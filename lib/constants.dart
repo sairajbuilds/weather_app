43891af32d2d82190ca28fd45e133dedb9596618
sairaj15 <sairajssawant@proton.me>
2026-06-28 11:47:33 +0530

    final commit

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const String weatherUrl =
+    'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$weatherApiKey';
+const String cityName = 'Mumbai';
+const String weatherApiKey = '5fb747747f17c591880a8c201df787ae';
